dde810e41be6dcfdd14e75bced7b6de8ecd05624
thefrollickingnerd <jack.moran192@gmail.com>
2020-08-17 00:26:48 +0930

    Added forward propagation function

--- neural_net/utils.py
+++ neural_net/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.random import randint
 
 
-def activation_func(x):
+def activation_func(x, func):
     """
     Apply sigmoid function on input. Can apply to matrices.
 
@@ -12,27 +12,33 @@ def activation_func(x):
     Returns:
         [x]: Same 2, size as input, sigmoid applied.
     """
-    sigmoid = 1 / (1 + np.exp(-x))
+    if func:
+        sigmoid = 1 / (1 + np.exp(-x))
+    else:
+        sigmoid = max(0, x)
+
     return sigmoid
 
 
+# TODO: Consider ReLU activation
+
 # def gradient_desc():
-def create_nodes(n_layers, n_nodes_hl, n_features):
-    """Generate network of nodes. 
+# def create_nodes(n_layers, n_nodes_hl, n_features):
+#     """Generate network of nodes.
 
-    Args:
-        n_layers ([type]): Number of hidden layers
-        n_nodes_hl ([type]): Nodes in hidden layers
-        n_features ([type]): Number of input features
-    """
-    network = [[randint(2, size=(n_features, 1))]]
-    for i in range(1, n_layers + 1):
-        network.append(randint(2, size=(n_nodes_hl, 1)))
-    network.append(randint(2, size=(1, 1)))
-    return network
+#     Args:
+#         n_layers ([type]): Number of hidden layers
+#         n_nodes_hl ([type]): Nodes in hidden layers
+#         n_features ([type]): Number of input features
+#     """
+#     network = [[randint(2, size=(n_features, 1))]]
+#     for i in range(1, n_layers + 1):
+#         network.append(randint(2, size=(n_nodes_hl, 1)))
+#     network.append(randint(2, size=(1, 1)))
+#     return network
 
 
-def create_theta_dict(n_layers, n_nodes_hl, n_features):
+def create_theta_dict(n_features, n_layers, n_nodes_hl, n_outnodes):
     """
     Create theta matrices inside dictionary. 
     Built on the assumption the final layer has 1 node. 
@@ -42,16 +48,30 @@ def create_theta_dict(n_layers, n_nodes_hl, n_features):
     thetas[0] = randint(2, size=(n_nodes_hl, n_features))
     for i in range(1, n_layers):
         thetas[i] = randint(2, size=(n_nodes_hl, n_nodes_hl))
-    thetas[n_layers] = randint(2, size=(1, n_nodes_hl))
+    thetas[n_layers] = randint(2, size=(n_outnodes, n_nodes_hl))
     return thetas
 
 
 def forward_propagation(features, thetas):
 
-    for i in range(0,len(thetas)):
-        
+    network = [features]
+    for i in range(0, len(thetas)):
+        Z = thetas[i] @ network[i]
+        A = activation_func(Z, True)
+        network.append(A)
+
+    return network
 
 
-print(create_theta_dict(2, 3, 1))
-# network = create_nodes(2, 3, 1)
-# print(network)
+n_features = 2
+n_layers = 2
+n_nodes_hl = 3
+n_outnodes = 2
+thetas = create_theta_dict(n_features, n_layers, n_nodes_hl, n_outnodes)
+features = randint(2, size=(n_features, 1))
+# print(f"thetas = {thetas} \n")
+print(f"features = {features} \n")
+# print(thetas[0] @ features)
+network = forward_propagation(features, thetas)
+print(f"After forward propagation neural net is : \n {network}")
+
