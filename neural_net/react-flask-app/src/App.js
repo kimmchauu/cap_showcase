0509c0bc783bf4386d4e00ab43b0a079236fddcb
Kim Chau <kimmchauu@gmail.com>
2020-09-04 21:10:56 +0930

    Add code to home page to show how we can invoke flask api endpoint and display it in the front end

--- neural_net/react-flask-app/src/App.js
+++ neural_net/react-flask-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 
 import Navigation from './components/navigation'
@@ -6,21 +6,6 @@ import PageRenderer from '../src/PageRenderer'
 
 
 function App() {
-  /* 
-  //can now use {currentTime} anywhere in html code
-  const[currentTime, setCurrentTime] = useState(0);
-
-  // function that is invoked when page is render, empty list means no dependencies, so it stops it from changing evrytime the time changes
-  useEffect(() => {
-      // get api call using fetch
-      // get response, convert to json, then get data
-      fetch('/time').then(res => res.json()).then(data => {
-          // logic here
-          // format of object was key and value (time)
-          setCurrentTime(data.time);
-      });
-  }, []);
-  */
 
   return (
     <Router>
--- neural_net/react-flask-app/src/pages/home.js
+++ neural_net/react-flask-app/src/pages/home.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 function Home() {
+
+    //can now use {currentTime} anywhere in html code - REMOVE THIS LATER this just shows how we can use flask and react
+    const[currentTime, setCurrentTime] = useState(0);
+
+    // function that is invoked when page is render, empty list means no dependencies, so it stops it from changing evrytime the time changes
+    useEffect(() => {
+        // get api call using fetch
+        // get response, convert to json, then get data
+        fetch('/time').then(res => res.json()).then(data => {
+            // logic here
+            // format of object was key and value (time)
+            setCurrentTime(data.time);
+        });
+    }, []);
+    
     return (
-        <div>Home</div>
+        <div>
+            <span>Home</span>
+            <p>The current time is {currentTime}</p>
+        </div>
     )
 }
 
