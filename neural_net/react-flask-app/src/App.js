c530d0d86942394e3a959cb7bca016c3b59f79db
Kim Chau <kimmchauu@gmail.com>
2020-09-04 19:57:35 +0930

    Deleted some files we don't need that comes with default app. Also, added in the tab title to I&D Showcase which can be changed in index.html

--- neural_net/react-flask-app/src/App.js
+++ neural_net/react-flask-app/src/App.js
@@ -1,11 +1,9 @@
 import React, {useState, useEffect} from 'react';
 
-import logo from './logo.svg';
-import './App.css';
-
-
 
 function App() {
+  /* 
+  //can now use {currentTime} anywhere in html code
   const[currentTime, setCurrentTime] = useState(0);
 
   // function that is invoked when page is render, empty list means no dependencies, so it stops it from changing evrytime the time changes
@@ -18,25 +16,11 @@ function App() {
           setCurrentTime(data.time);
       });
   }, []);
+  */
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/*Renders in the front end here, shows the current time which is taken from fast api backend */}
-        <p>The current time is {currentTime}</p>
-      </header>
+
     </div>
   );
 }
