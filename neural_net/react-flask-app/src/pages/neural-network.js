10080ba1f9e926bb6456802e30258e5a790174f8
Kim Chau <kimmchauu@gmail.com>
2020-09-14 01:20:25 +0930

    Added an image header and text to neural network page. Looks cooler haha. Added type write effect for title

--- neural_net/react-flask-app/src/pages/neural-network.js
+++ neural_net/react-flask-app/src/pages/neural-network.js
@@ -7,9 +7,12 @@ import neuralInstructionCards from '../assets/mock-data/neural-network-instructi
 function NeuralNetwork() {
     return (
         <div>
+            <div className="header-text-container">
+                <img className="neural-header-image" src={require('../assets/images/unravel.png')} alt="neural header"/>
+                <h1 className="bottom-left">Unravelling the neural-network</h1>
+            </div>
             <section className="container neural-network">
                 <div className="row">
-                    <h1>Unravelling the neural-network</h1>
                     <h2>We have build a neural-network to ..</h2>
                     <p>Instructions: Train a neural network to recognise a colour by clicking a card below </p>
                     <CreateCard data={neuralInstructionCards} columns={2}/>
