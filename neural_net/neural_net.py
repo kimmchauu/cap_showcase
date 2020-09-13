5d41999fd8fd79f899796f18e373caec0831c195
thefrollickingnerd <jack.moran192@gmail.com>
2020-09-13 17:27:21 +0930

    Add error metric for training

--- neural_net/neural_net.py
+++ neural_net/neural_net.py
@@ -5,7 +5,7 @@ import math
 class NeuralNet:
     """A simple neural network. 
     """
-    def __init__(self, model_name, n_layers, n_features, n_nodes_hl): 
+    def __init__(self, model_name): 
         """
         Args:
             model_name (string): name of model
@@ -14,15 +14,18 @@ class NeuralNet:
             n_nodes_hl (int): Number of nodes in hidden layers
         """
         self.model_name = model_name
+        
+    def set_model(self,n_layers, n_features, n_nodes_hl):
         self.n_layers = n_layers
         self.n_features = n_features
         self.n_nodes_hl = n_nodes_hl
         
+    def get_network(self):
+        return self.network 
 
     def train_model(self):
     
-    
-    
+    def predict(self):
     
     
     
