7b1bb045c4f8018126f4a3a3d3dd4b9e5e9df057
Kim Chau <kimmchauu@gmail.com>
2020-09-13 16:22:52 +0930

    Captures the data of the colour that is selected and consoles that to log for testing

--- neural_net/react-flask-app/src/pages/select-a-colour.js
+++ neural_net/react-flask-app/src/pages/select-a-colour.js
@@ -4,6 +4,21 @@ import data from '../assets/data/colours.json'
 
 function SelectAColour() {
     const [colourData, setColourData] = useState(data);
+    const [colourSelected, setColourSelected] = useState();
+
+    function colorSelected(colour) {
+        // shows the correct colour has been selected and data captured
+        console.log("colour obj selected: ", colour)
+        console.log("colour name: ", colour.Name)
+        console.log("colour hex: ", colour["Hex (24 bit)"])
+        console.log("colour red: ", colour["Red (8 bit)"])
+        console.log("colour green: ", colour["Green (8 bit)"])
+        console.log("colour blue: ", colour["Blue (8 bit)"])
+
+        // now change use colourSelected to pass to other components if need be
+        setColourSelected(colour)
+    }
+    
 
     // when import after use map() method we can loop all the items in the JSON file.
     // Map() method used only for the array but in our JSON file format is of object type [{}, {}]
@@ -14,13 +29,12 @@ function SelectAColour() {
                     <h1>Select a Colour ({colourData.length} colours)</h1>
                     {   
                         Object.values(colourData).map(colour => {
-                        var color = colour["Hex (24 bit)"]
                         //document.getElementById("colour-button").style.backgroundColor = color;
                         return (
                             <div className="colour-container">
                                     <div className="col-xs-1">
                                         {/* Need to display the colours but also capture what colour was selected */}
-                                        <button className="colour-button" style={{background: color}}></button>
+                                        <button className="colour-button" style={{background: colour["Hex (24 bit)"]}} onClick={() => colorSelected(colour)}></button>
                                     </div>
                                 
                             </div> 
