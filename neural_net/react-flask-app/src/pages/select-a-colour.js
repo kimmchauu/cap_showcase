45e69d71e6e96e6c02ba9d2ebfb9b24119174731
Kim Chau <kim.chau@capgemini.com>
2020-09-13 16:09:18 +0930

    Add the colours to the select a colour page as rows of 12, using buttons. Set the button background colour to the colour. Will need to add functionality for when a user selects a button, that data is capture i.e what colour was chosen

--- neural_net/react-flask-app/src/pages/select-a-colour.js
+++ neural_net/react-flask-app/src/pages/select-a-colour.js
@@ -7,20 +7,22 @@ function SelectAColour() {
 
     // when import after use map() method we can loop all the items in the JSON file.
     // Map() method used only for the array but in our JSON file format is of object type [{}, {}]
-    console.log(colourData[0].Name)
     return (
         <>
             <section className="container neural-network">
                 <div className="row">
-                    <h1>Select a Colour</h1>
+                    <h1>Select a Colour ({colourData.length} colours)</h1>
                     {   
                         Object.values(colourData).map(colour => {
+                        var color = colour["Hex (24 bit)"]
+                        //document.getElementById("colour-button").style.backgroundColor = color;
                         return (
-                            <div>
-                            
-                                {/* Need to display the colours but also capture what colour was selected */}
-                                <p>{colour.Name}</p>
-                                    
+                            <div className="colour-container">
+                                    <div className="col-xs-1">
+                                        {/* Need to display the colours but also capture what colour was selected */}
+                                        <button className="colour-button" style={{background: color}}></button>
+                                    </div>
+                                
                             </div> 
                         );
                         })
