ba1277ee6de9a1e2908edfa95316133aaee1b431
Kim Chau <kimmchauu@gmail.com>
2020-09-13 17:00:29 +0930

    Removing imports not needed

--- neural_net/react-flask-app/src/pages/select-a-colour.js
+++ neural_net/react-flask-app/src/pages/select-a-colour.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Link } from 'react-router'
 
 import data from '../assets/data/colours.json'
 
