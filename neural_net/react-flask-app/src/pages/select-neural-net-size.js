f5d17a98978195c3b235c5ddd55cd6380687965d
Kim Chau <kimmchauu@gmail.com>
2020-09-13 23:24:51 +0930

    Add images for neural net sizes: 1-3 hidden layers and 2-3 nodes per layer. Add the correct neural net image to show based on the user's interaction. Not the best way to do this, but it is what it isss

--- neural_net/react-flask-app/src/pages/select-neural-net-size.js
+++ neural_net/react-flask-app/src/pages/select-neural-net-size.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 
@@ -8,12 +8,12 @@ function SelectNeuralNetSize(props) {
     const [hiddenLayers, setHiddenLayers] = useState(1); // 1 hidden layer as default
     const [nodes, setNodes] = useState(2);
 
+
     const incrementLayers = () => {
         if (hiddenLayers < 3 ){
             setHiddenLayers(hiddenLayers + 1);
         }
     }
-
     const decrementLayers = () => {
         if (hiddenLayers > 1 ){
             setHiddenLayers(hiddenLayers - 1);
@@ -25,12 +25,36 @@ function SelectNeuralNetSize(props) {
             setNodes(nodes + 1);
         }
     }
-
     const decrementNodes = () => {
         if (nodes > 2 ){
             setNodes(nodes - 1);
         }
     }
+
+    // useEffect is triggered whenever the depedencies are changed (hiddenLayers, nodes)
+    useEffect(() => {
+        // based on the dependencies (hiddenLayers, nodes), show the correct image
+        if (hiddenLayers === 1){
+            if (nodes === 2){
+                document.getElementById("neural-net-size-image").src = require("../assets/images/1layer_2nodes.png");
+            } else if (nodes === 3){
+                document.getElementById("neural-net-size-image").src = require("../assets/images/1layer_3nodes.png");
+            } 
+        } else if (hiddenLayers === 2) {
+            if (nodes === 2){
+                document.getElementById("neural-net-size-image").src = require("../assets/images/2layers_2nodes.png");
+            } else if (nodes === 3){
+                document.getElementById("neural-net-size-image").src = require("../assets/images/2layers_3nodes.png");
+            }
+        }else if (hiddenLayers === 3) {
+            if (nodes === 2){
+                document.getElementById("neural-net-size-image").src = require("../assets/images/3layers_2nodes.png");
+            } else if (nodes === 3){
+                document.getElementById("neural-net-size-image").src = require("../assets/images/3layers_3nodes.png");
+            } 
+        }
+    }, [hiddenLayers, nodes])
+
     // delete this after - shows how to access data needed
     // console.log("colour obj selected: ", location.state.colourSelected)
     // console.log("colour name: ", location.state.colourSelected.Name)
@@ -47,6 +71,8 @@ function SelectNeuralNetSize(props) {
                         <h1>Colour Selected: {location.state.colourSelected.Name}. Red - {location.state.colourSelected["Red (8 bit)"]}, Green: {location.state.colourSelected["Green (8 bit)"]} , Blue: {location.state.colourSelected["Blue (8 bit)"]} </h1>
                     </div>
                     <h1>Select a Neural Network Size</h1>
+
+                    {/*  + - buttons for hidden layers and nodes per hidden layer  */}
                     <div className="row">
                         <div className="col-xs-1">
                             <button className="plus-minus-button" onClick={incrementLayers}>
@@ -74,6 +100,11 @@ function SelectNeuralNetSize(props) {
                         <div className="col-xs-4">
                             <p className="hidden-layers-title">{nodes} nodes per hidden layer (2-3)</p>
                         </div>
+                    </div>
+
+                    {/* Shows the neural net size based user selection */}
+                    <div className="row">
+                        <img id="neural-net-size-image" src={require('../assets/images/1layer_2nodes.png')} alt="neural net size"/>
                     </div>
                 </div>
             </section>  
